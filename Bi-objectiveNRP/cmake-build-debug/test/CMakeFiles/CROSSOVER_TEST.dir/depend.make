9c20978abc226c704774df1c2758c5f87a9307dc
Giovanni Daián Rottoli <gd.rottoli@gmail.com>
2018-12-19 18:17:53 -0300

    Agregados archivos de idea al gitignore

--- Bi-objectiveNRP/cmake-build-debug/test/CMakeFiles/CROSSOVER_TEST.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CROSSOVER_TEST.
-# This may be replaced when dependencies are built.
--- Bi-objectiveNRP/cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carlos/CLionProjects/GIICIS/Bi-objectiveNRP/test
-# Build directory: /home/carlos/CLionProjects/GIICIS/Bi-objectiveNRP/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
